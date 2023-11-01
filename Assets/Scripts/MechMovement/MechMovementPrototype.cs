51cb887950c2eb75b45bb386c239f48e0500e194
ADJ-acent <weiwenj@andrew.cmu.edu>
2023-11-01 11:49:39 -0400

    mech rotation completed

--- Assets/Scripts/MechMovement/MechMovementPrototype.cs
+++ Assets/Scripts/MechMovement/MechMovementPrototype.cs
@@ -9,10 +9,9 @@ public class MechMovementPrototype : MonoBehaviour
     public Transform robotRigTransform;
 
     public float angleLimit;
-
-    public float rotationMaxSpeed;
+    // Max Angle to rotate per tick
+    public float rotationMaxAngle;
     
-    private Vector3 rotateVelocity = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,37 +21,14 @@ public class MechMovementPrototype : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Quaternion headsetQuaternion = vrHeadTransform.localRotation;
+        Quaternion headsetQuaternion = vrHeadTransform.rotation;
         Vector3 headsetVector3 = headsetQuaternion.eulerAngles;
         //prevent weird head movement when close to purely up or down
-        if (Vector3.Angle(Vector3.down, headsetVector3) > angleLimit && 
-            Vector3.Angle(Vector3.up, headsetVector3) > angleLimit)
+        if (Vector3.Angle(Vector3.down, vrHeadTransform.forward) > angleLimit && 
+            Vector3.Angle(Vector3.up, vrHeadTransform.forward) > angleLimit)
         {
-            //project the headsetVector to the xz plane
-            Vector3 projectedVector = Vector3.ProjectOnPlane(headsetVector3, Vector3.up);
-            Vector3 robotVector = robotRigTransform.eulerAngles;
-            robotRigTransform.localRotation = Quaternion.FromToRotation(robotVector, projectedVector);
+            robotRigTransform.rotation = Quaternion.RotateTowards(robotRigTransform.rotation, 
+                Quaternion.Euler(0f, headsetVector3.y,0), rotationMaxAngle); 
         }
-    }
-    
-    //adapted from https://forum.unity.com/threads/quaternion-to-remove-pitch.822768/
-    public static Quaternion GetXAxisRotation( Quaternion quaternion)
-    {
-        float a = Mathf.Sqrt((quaternion.w * quaternion.w) + (quaternion.x * quaternion.x));
-        return new Quaternion(x: quaternion.x, y: 0, z: 0, w: quaternion.w / a);
- 
-    }
- 
-    public static Quaternion GetYAxisRotation( Quaternion quaternion)
-    {
-        float a = Mathf.Sqrt((quaternion.w * quaternion.w) + (quaternion.y * quaternion.y));
-        return new Quaternion (x: 0, y: quaternion.y, z: 0, w: quaternion.w / a);
- 
-    }
- 
-    public static Quaternion GetZAxisRotation( Quaternion quaternion)
-    {
-        float a = Mathf.Sqrt((quaternion.w * quaternion.w) + (quaternion.z * quaternion.z));
-        return new Quaternion(x: 0, y: 0, z: quaternion.z, w: quaternion.w / a);
     }
 }
