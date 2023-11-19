255cd50954b1f037f382f79663fff566468ccb33
HelenYang1031 <yangzixin20011031@163.com>
2023-11-19 00:33:36 -0500

    ahhhhh Maya synchronization lost

--- Assets/Crabb/Script/rotateBody.cs
+++ Assets/Crabb/Script/rotateBody.cs
@@ -6,13 +6,13 @@ public class rotateBody : MonoBehaviour
 {
     void Update()
     {
-        RaycastHit hit;     //射线
+        RaycastHit hit;     //ray
         int Rmask = LayerMask.GetMask("ik");
         Vector3 Point_dir = transform.TransformDirection(Vector3.down);
         if (Physics.Raycast(transform.position, Point_dir, out hit, 50.0f, Rmask))
         {
             Quaternion NextRot = Quaternion.LookRotation(Vector3.Cross(hit.normal, Vector3.Cross(transform.forward, hit.normal)), hit.normal);
-            GetComponent<Rigidbody>().MoveRotation(Quaternion.Lerp(transform.rotation, NextRot, 0.1f)); //旋转
+            GetComponent<Rigidbody>().MoveRotation(Quaternion.Lerp(transform.rotation, NextRot, 0.1f)); //rotate
         }
 
     }
