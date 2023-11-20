46482b30ce8a4ea33662730984e5dfcadb7bcfe0
ADJ-acent <weiwenj@andrew.cmu.edu>
2023-11-20 01:47:39 -0500

    half of AI tree done
    I wanna die

--- Assets/Scripts/BossAI/BossBT.cs
+++ Assets/Scripts/BossAI/BossBT.cs
@@ -6,10 +6,26 @@ namespace BossAI
 {
     public class BossBT : BTree
     {
+
         public Transform mechTransform;
+        public Transform[] buildingTransforms;
+        public float attackRange = 5f;
         protected override Node SetupTree()
         {
-            return new Node();
+            Transform t = transform;
+            return new Selector(new List<Node> {
+                new Sequence(new List<Node>
+                {
+                    new CheckBuildingInAttackRange(t, attackRange),
+                    new TaskAttackBuilding(t)
+                }),
+                
+                new Sequence(new List<Node>
+            {
+                new CheckRemainBuilding(t, buildingTransforms),
+                new TaskGoToBuilding(t),
+            }), 
+            new TaskWander(t,50f)});
         }
     }
 
--- Assets/Scripts/BossAI/TaskWander.cs
+++ Assets/Scripts/BossAI/TaskWander.cs
@@ -3,55 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using BehaviorTree;
+using UnityEngine.AI;
 
 namespace BossAI
 {
-    public class TaskPatrol : Node
+    public class TaskWander : Node
     {
         private Transform _transform;
         private Animator _animator;
+        private NavMeshAgent _navMeshAgent;
+        private const float _waitTime = 3f; // in seconds
+        private float _counter = 0f;
+        private bool _waiting = true;
+         private readonly float _wanderDistance;
 
-        private int _currentWaypointIndex = 0;
-
-        private float _waitTime = 1f; // in seconds
-        private float _waitCounter = 0f;
-        private bool _waiting = false;
-
-        public TaskPatrol(Transform transform, Transform[] waypoints)
+        public TaskWander(Transform transform, float wanderDistance)
         {
             _transform = transform;
             _animator = transform.GetComponent<Animator>();
+            _navMeshAgent = transform.GetComponent<NavMeshAgent>();
+            _wanderDistance = wanderDistance;
         }
 
         public override NodeState Evaluate()
         {
             if (_waiting)
             {
-                _waitCounter += Time.deltaTime;
-                if (_waitCounter >= _waitTime)
+                _counter += Time.deltaTime;
+                if (_counter >= _waitTime)
                 {
                     _waiting = false;
-                    _animator.SetBool("Walking", true);
+                    _animator.SetTrigger("Idle");
+                    _counter = 0;
+                    // go in a random direction
+                    Vector2 randomDir = Random.insideUnitCircle * _wanderDistance;
+                    NavMeshHit hit;
+                    if (NavMesh.SamplePosition(new Vector3(randomDir.x, 0, randomDir.y) + _transform.position,
+                            out hit, 10.0f, NavMesh.AllAreas))
+                    {
+                        _navMeshAgent.SetDestination(hit.position);
+                    }
+                    
                 }
             }
             else
             {
-                /*Transform wp = _waypoints[_currentWaypointIndex];
-                if (Vector3.Distance(_transform.position, wp.position) < 0.01f)
+                // Check if agent reached the destination
+                if (!_navMeshAgent.pathPending)
                 {
-                    _transform.position = wp.position;
-                    _waitCounter = 0f;
-                    _waiting = true;
-
-                    _currentWaypointIndex = (_currentWaypointIndex + 1) % _waypoints.Length;
-                    _animator.SetBool("Walking", false);
+                    if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+                    {
+                        if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f)
+                        {
+                            _waiting = true;
+                            _animator.SetTrigger("Dance");
+                            _counter = 0;
+                        }
+                    }
                 }
-                else
-                {
-                    _transform.position =
-                        Vector3.MoveTowards(_transform.position, wp.position, GuardBT.speed * Time.deltaTime);
-                    _transform.LookAt(wp.position);
-                }*/
             }
 
 
